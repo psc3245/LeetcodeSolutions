a76a26272939090222f5995fe4896d14b770ff75
psc3245 <psc3.omaha@gmail.com>
2025-01-13 14:58:34 -0600

    2 meds

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,17 @@
-
+import Medium.K_Closest_Points_973.Solution;
 
 public class Main {
     public static void main(String[] args) {
+        int[][] case1 = { {0, 2}, {2, 0}, {2, 2} };
+        int[][] sol1 = Solution.kClosest(case1, 2);
+        System.out.println("Case 1: ");
+        for (int[] arr : sol1) System.out.println(arr[0] + ", " + arr[1]);
+
+        int[][] case2 = { {2, 3}, {0, 2}, {2, 2} };
+        int[][] sol2 = Solution.kClosest(case2, 1);
+        System.out.println("Case 2: ");
+        for (int[] arr : sol2) System.out.println(arr[0] + ", " + arr[1]);
+
 
     }
 
