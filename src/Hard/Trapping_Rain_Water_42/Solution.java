32d8d6fab3d82a06ed92bbd20efe475c9ccf9c58
psc3245 <psc3.omaha@gmail.com>
2025-01-29 15:11:43 -0600

    2m

---
+++ src/Hard/Trapping_Rain_Water_42/Solution.java
@@ -0,0 +1,11 @@
+package Hard.Trapping_Rain_Water_42;
+
+public class Solution {
+    public int trap(int[] height) {
+        int total;
+        int l = 0;
+        int r = 1;
+
+        return 0;
+    }
+}
