bf98ceb83236a22b7364506f4a07f545f8f7002e
psc3245 <psc3.omaha@gmail.com>
2026-01-03 13:36:29 -0600

    1m

---
+++ python/Medium/877_stone_game.py
@@ -0,0 +1,5 @@
+class Solution:
+    def stoneGame(self, piles: List[int]) -> bool:
+        return True
+        # Alice can ensure that she can pick either all even or all odd
+        # Since total is odd one of those is guaranteed to be greater
